5da554d42a5e27e16fb5107bb18f4ec731d75158
milton1 <livemilton@hotmail.com>
2022-08-22 19:37:05 -0500

    hola mundo

--- src/main/java/com/Udea/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/Udea/Ciclo3/Ciclo3Application.java
@@ -1,5 +1,6 @@
 package com.Udea.Ciclo3;
 
+import com.Udea.Ciclo3.modelos.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -14,6 +15,20 @@ public class Ciclo3Application {
 	public String hello(){
 		return "Hola Ciclo 3... Saldremos vivos de esto!";
 	}
+
+	@GetMapping ("/test")
+	public String test(){
+		Empresa emp = new Empresa("Espada De Bolivar","calle 48 a sur n 88C", "7774678" , "122548965-3");
+		emp.setNombre("Espada de Bolivar Ltda");
+
+		System.out.println("Aqui ya se creo la empresa y se renombro");
+		return emp.getNombre();
+
+	}
+
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
 
